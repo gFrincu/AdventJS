42d34fc274947aaff856b7e4d3e940aecb39d8c6
Georgiana Frincu <gfrincuh@gmail.com>
2023-12-06 14:58:00 +0100

    Code for day 6

---
+++ dia6.ts
@@ -0,0 +1,40 @@
+function maxDistance(movements: string): number {
+	let i = 0, counter = 0, asterisks = 0;
+	while (i < movements.length) {
+		if (movements[i] == '>') {
+			counter++;
+		}
+		if (movements[i] == '<') {
+			counter--;
+		}
+		if (movements[i] == '*') {
+			asterisks++
+		}
+		i++;
+	}
+	if (Math.abs(counter + asterisks) >= Math.abs(counter - asterisks)) {
+		counter = Math.abs(counter + asterisks);
+	}
+	else {
+		counter = Math.abs(counter - asterisks);
+	}
+
+	return counter;
+}
+
+const movements = '>>*<'
+const result = maxDistance(movements)
+console.log(result) // -> 2
+
+const movements2 = '<<<>'
+const result2 = maxDistance(movements2)
+console.log(result2) // -> 2
+
+const movements3 = '>***>'
+const result3 = maxDistance(movements3)
+console.log(result3) // -> 5
+
+// En este programa lo que he hecho es que para < y > retroceda o avance según sea el caso
+// En cuanto a los asteriscos, como depende de la distancia y lo que beneficie, pues he decidido contar cuantos hay con un contador de asteriscos
+// Entonces como lo que necesitamos es el valor absoluto de la distancia, le he dicho que me diga cuando sería el valor absoluto sumando los asteriscos, y restando los asteriscos
+//  El que sea más grande, es con el que me quedo
